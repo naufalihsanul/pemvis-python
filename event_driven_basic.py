04b598b38b8fc71fc7619132d5528e469c76e2ee
Naufal72k <naufal72k@gmail.com>
2026-03-29 09:41:02 +0800

    belajar lagi

--- event_driven_basic.py
+++ event_driven_basic.py
@@ -1,63 +1,53 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QLabel, QPushButton,QLineEdit,QVBoxLayout,QWidget
-from PySide6.QtCore import Qt
-
-class MainWindow (QWidget):
-
-
+from PySide6.QtWidgets import *
+from PySide6.QtCore import Qt, QTimer
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setup_ui()
-        
 
     def setup_ui(self):
-        self.setWindowTitle("nyoba")
+        self.setWindowTitle("ini aplikasi")
         self.resize(400,300)
 
         self.labelEmail = QLabel("Email: ")
-        self.inputEmail: QLineEdit = QLineEdit()
-        
+        self.inputEmail = QLineEdit()
         self.labelPassword = QLabel("Password: ")
-        self.inputPassword: QLineEdit = QLineEdit()
-        self.inputPassword.setEchoMode(QLineEdit.EchoMode.Password)
+        self.inputPassword = QLineEdit()
+        self.buttonLogin = QPushButton("Login")
 
-        self.buttonLogin: QPushButton = QPushButton("Login")
 
         layout = QVBoxLayout()
-        layoutCenter = QVBoxLayout()
 
-        self.buttonLogin.clicked.connect(self.cekNama)
-
-        
         layout.addWidget(self.labelEmail)
         layout.addWidget(self.inputEmail)
         layout.addWidget(self.labelPassword)
         layout.addWidget(self.inputPassword)
         layout.addWidget(self.buttonLogin)
 
+
+        self.buttonLogin.clicked.connect(self.cekLogin)
+
         layout.setAlignment(Qt.AlignmentFlag.AlignTop)
 
-        layoutCenter.addLayout(layout)
 
-        layoutCenter.setAlignment(Qt.AlignTop)
-
-        self.setLayout(layoutCenter)
-
-    def cekNama(self):
-        if self.inputEmail.text() == "admin" and self.inputPassword.text() == "123456":
+        self.setLayout(layout)
+    
+    def cekLogin(self):
+        if self.inputEmail.text() == "admin" and self.inputPassword.text() == "12345678":
             self.buttonLogin.setText("Login berhasil")
         else:
-            self.buttonLogin.setText("email atau password salah")
+            self.buttonLogin.setText("password dan email salah")
+        
+       
+        self.inputEmail.setText("")
+        self.inputPassword.setText("")
 
-app  = QApplication(sys.argv)
+        # Kembalikan teks button ke "Login" setelah 3 detik (3000 ms)
+        QTimer.singleShot(3000, lambda: self.buttonLogin.setText("Login"))
+
+app = QApplication(sys.argv)
 window = MainWindow()
-
 window.show()
-
 sys.exit(app.exec())
-
-
-        
-        
-    
